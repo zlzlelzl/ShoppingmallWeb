bb7b27a6d250fc7dba2f9aa58a1a1073e493ce78
zlzlelzl <wkdwl1234@naver.com>
2021-07-18 19:57:57 +0900

    1

--- Shoppingmall/Shoppingmall/urls.py
+++ Shoppingmall/Shoppingmall/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from testpage.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("write/", write, name="write")
 ]
