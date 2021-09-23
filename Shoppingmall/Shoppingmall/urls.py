0ec8dda229063fdf26b4af3f1bba2b95d01f2352
zlzlelzl <wkdwl1234@naver.com>
2021-09-23 09:58:46 +0900

    1

--- Shoppingmall/Shoppingmall/urls.py
+++ Shoppingmall/Shoppingmall/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from testpage.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("write/", write, name="write")
+    path("api/v1/", include("djoser.urls")),
+    path("api/v1/", include("djoser.urls.authtoken"))
 ]
