d30acc61800ebed4d5c61d4d1fe0d3bd4d816d44
zlzlelzl <wkdwl1234@naver.com>
2021-09-25 20:55:28 +0900

    make category detail

--- Shoppingmall/shoppingmall_vue/src/views/Home.vue
+++ Shoppingmall/shoppingmall_vue/src/views/Home.vue
@@ -17,28 +17,18 @@
                 <h2 class="is-size-2 has-text-centered">Latest products</h2>
             </div>
 
-            <div class="column is-3"
+            <ProductBox
                  v-for="product in latestProducts"
                  v-bind:key="product.id"
-            >
-                <div class="box">
-                    <figure class="image mb-4">
-                        <img :src="product.get_thumbnail">
-                    </figure>
-
-                    <h3 class="is-size-4">{{ product.name }}</h3>
-                    <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-
-                    <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-                </div>
-            </div>
-
+                 v-bind:product="product" />
         </div>
     </div>
 </template>
 
 <script>
 import axios from "axios"
+
+import ProductBox from "@/components/ProductBox"
 
 export default {
     name: "Home",
@@ -48,13 +38,18 @@ export default {
         }
     },
     components: {
+        ProductBox
     },
     mounted(){
         this.getLatestProducts()
+
+        document.title = "Home | Djackets"
     },
     methods:{
-        getLatestProducts(){
-            axios
+        async getLatestProducts(){
+            this.$store.commit("setIsLoading",true)
+
+            await axios
                 .get("/api/v1/latest-products/")
                 .then(response => {
                     this.latestProducts = response.data
@@ -62,15 +57,9 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+
+            this.$store.commit("setIsLoading",false)
         }
     }
 }
 </script>
-
-<style scoped>
-    .image {
-        margin-top: -1.25rem;
-        margin-left: -1.25rem;
-        margin-right: -1.25rem;
-    }
-</style>
