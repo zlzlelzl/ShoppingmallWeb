387a02536aefb95bbcc405a7fe59437be38c8f9d
zlzlelzl <wkdwl1234@naver.com>
2021-10-01 15:38:43 +0900

    mod database

--- Shoppingmall/Shoppingmall/settings.py
+++ Shoppingmall/Shoppingmall/settings.py
@@ -90,10 +90,20 @@ WSGI_APPLICATION = 'Shoppingmall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "djackets",
+        'USER': "djacketsuser",
+        "PASSWORD": "djacketsuser",
+        "HOST": "localhost",
+        "PORT": ""
     }
 }
 
